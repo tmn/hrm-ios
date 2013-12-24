7cab91edbac6d1d665b17e24a81aae747436247a
Tri M. Nguyen <mail@trimn.net>
2013-12-24 03:23:52 +0100

    Initial Commit

---
+++ HRM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HRM
+//
+//  Created by Tri M. Nguyen on 24/12/13.
+//  Copyright (c) 2013 Tri M. Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HRM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HRM
+//
+//  Created by Tri M. Nguyen on 24/12/13.
+//  Copyright (c) 2013 Tri M. Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
